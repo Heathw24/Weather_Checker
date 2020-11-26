85e30cb8fa4200704898d082a72ec0361f6fda67
Heath Ward <heathw24@outlook.com>
2020-11-25 21:19:56 -0500

    started display function

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,5 +1,5 @@
 
-// retrieve user input and convert to variables
+
 
 
 
@@ -8,6 +8,10 @@ var input = $("#locationInput");
 var convertedInput = input[0];
 
 locationSearch[0].addEventListener('submit', pullInfo);
+
+
+// retrieve user input and convert to variables
+//==================================================================================================
 
 function pullInfo() {
 event.preventDefault();
@@ -18,15 +22,31 @@ var history = [];
 
 console.log(currWeatherURl);
 
+
+// Use input to run ajax call to APIs
+//=======================================================================================================
+
 $.ajax({
     url:currWeatherURl,
     method: "GET"
 }).then(function(response){
     console.log(response)
+
+displayInfo();
 })
 
+
+
+function displayInfo(){
+    var dateEl = document.getElementById("date");
+    dateEl.innerHTML = now;
+    var cityEl = document.getElementById("cityName");
+    console.log(cityEl);
+    console.log(city);
+    cityEl.innerHTML = city;
 }
 
+}
 // Break down API Object into useable fields
 
 // Create html and display data on page
