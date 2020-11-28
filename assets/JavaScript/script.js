89dbd2f622363dbfaa0942f9c37bcd4299e11b59
Heath Ward <heathw24@outlook.com>
2020-11-27 21:50:25 -0500

    pulled current data from api to display

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,6 +1,7 @@
 
 
-
+var now = moment().format('l');
+console.log(now);
 
 
 var locationSearch = $("#searchArea");
@@ -32,21 +33,85 @@ $.ajax({
 }).then(function(response){
     console.log(response)
 
-displayInfo();
+  // this pulls the latitude and longitude from the first api call to call the second api
+    var lat = response.coord.lat;
+    console.log(lat);
+    var lon = response.coord.lon;
+    console.log(lon);
+
+  var fullWeatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + weatherKey;
+  console.log(fullWeatherURL);
+
+    $.ajax({
+        url: fullWeatherURL,
+        method: "GET"
+    }) .then(function(fullresponse){
+
+       console.log(fullresponse);
+
+// Break down API Object into useable fields
+
+           // current Temprature
+       var temp = fullresponse.current.temp
+       var curTemp = document.createElement("p");
+       curTemp.innerHTML = "Temperature:" + " " + temp;
+
+            // current Humidity
+       var humi = fullresponse.current.humidity;
+       var curHumi = document.createElement("p");
+       curHumi.innerHTML = "Humidity:" + " " + humi;
+
+            // current Windspeed
+        var windS = fullresponse.current.wind_speed;
+        var curWindS = document.createElement("p");
+        curWindS.innerHTML = "Wind Speed:" + " " + windS;
+
+            // current UV index
+        var uv = fullresponse.current.uvi;
+        var curUV = document.createElement("p");
+        curUV.innerHTML = "UV Index:" + " " + uv;
+            
+
+        // day 1 forcast
+        var day1Weather = fullresponse.daily[0].weather[0].icon;
+        var d1URL = "http://openweathermap.org/img/w/" + day1Weather + ".png"
+        var day1W = document.createElement("img");
+        day1W.src = d1URL;
+
+        var d1temp 
+
+
+      displayInfo();
+
+
+
+// Create html and display data on page
+function displayInfo(){
+    var cityEl = document.getElementById("cityName");
+    cityEl.innerHTML = city + "   " + now;
+
+    var display = document.getElementById("display"); 
+    display.append(curTemp);
+    display.append(curHumi);
+    display.append(curWindS);
+    display.append(curUV);
+
+    var displayD1 = document.getElementById("forcast1");
+    displayD1.append(day1W);
+};
+
+    })
+
+
 })
 
 
 
-function displayInfo(){
-    var dateEl = document.getElementById("date");
-    dateEl.innerHTML = now;
-    var cityEl = document.getElementById("cityName");
-    console.log(cityEl);
-    console.log(city);
-    cityEl.innerHTML = city;
-}
+
+
+
+
 
 }
-// Break down API Object into useable fields
 
-// Create html and display data on page
+
